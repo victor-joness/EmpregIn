7fd44fcb5d309385a44d4d617dd34a8e49c8b67b
Victor Mesquita <victorjonesmesquits@gmail.com>
2024-07-27 11:57:44 -0300

    Update Home.jsx

--- Client/src/Pages/Home/Home.jsx
+++ Client/src/Pages/Home/Home.jsx
@@ -1,9 +1,56 @@
 import React from "react";
+import "./Home.css";
+
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import { googleSignIn } from "../../App-config-teste/user-slice";
+import { toast } from "react-toastify";
+
+/*________________________________________________________________________________*/
 
 const Home = () => {
-  return <div>
-    <h1>TESTE HOME</h1>
-  </div>;
+  const user = useSelector((state) => state.user.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="container">
+      {user && <Navigate to={"feed"} />}
+      <nav>
+        <a href="/">
+          <img src="/Images/login-logo.svg" alt="logo" />
+        </a>
+        <div>
+          {user ? (
+            <a href="/login" className="feed">
+              ir para o feed
+            </a>
+          ) : (
+            <>
+              <a href="/register" className="join">
+                Join now
+              </a>
+              <a href="/login" className="login">
+                Sign in
+              </a>
+            </>
+          )}
+        </div>
+      </nav>
+
+      <section>
+        <div className="hero">
+          <h1>Welcome to your professional community</h1>
+          <img src="/Images/hero.svg" alt="hero" />
+        </div>
+        <div className="form">
+          <button onClick={() => dispatch(googleSignIn())} className="google">
+            <img src="/Images/google.svg" alt="google" />
+            Sign in with Google
+          </button>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
