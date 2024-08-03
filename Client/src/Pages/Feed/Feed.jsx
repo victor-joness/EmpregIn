5bc9e3ce22d49a845bb60eebf74ef8eaf36fb427
Victor Mesquita <victorjonesmesquits@gmail.com>
2024-08-03 11:34:23 -0300

    Update Feed.jsx

--- Client/src/Pages/Feed/Feed.jsx
+++ Client/src/Pages/Feed/Feed.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import Feed_Perfil from "../../Components/Feed_Perfil/Feed_Perfil";
+import Feed_Main from "../../Components/Feed_Main/Feed_Main";
+import Feed_Recomendacao from "../../Components/Feed_Recomendacao/Feed_Recomendacao";
 import "./Feed.css";
 
-const Feed = () => {
+const Feed = (props) => {
+  document.title = "Feed | Linkedin";
   return (
-    <div className="feed">
-      <h1>TESTE FEED</h1>
+    <div className="container">
+      <div className="layout">
+        <Feed_Main />
+        <Feed_Perfil />
+        <Feed_Recomendacao />
+      </div>
     </div>
   );
 };
