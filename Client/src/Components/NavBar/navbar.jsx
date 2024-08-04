c1f7c02c2c9cc98492a70d4ea8c03f8a39275c50
Samuel Lopes <samuellopes12390@gmail.com>
2024-08-04 11:08:54 -0300

    feat: Componente NavBar Concluído

--- Client/src/Components/NavBar/navbar.jsx
+++ Client/src/Components/NavBar/navbar.jsx
@@ -2,6 +2,14 @@ import React from "react";
 
 import './navbar.css'
 
+import { FiUsers } from "react-icons/fi";
+import { BsSuitcaseLg } from "react-icons/bs";
+import { FaSistrix } from "react-icons/fa6";
+import { FaRss } from "react-icons/fa6";
+import { FaRegBell } from "react-icons/fa6";
+import { BiComment } from "react-icons/bi";
+import { MdArrowOutward } from "react-icons/md";
+
 const NavBar = () => {
   return (
     <div className="header">
@@ -10,34 +18,37 @@ const NavBar = () => {
         </div>
         <div className="box_icons_navigation">
         	<ul>
-            	<li><a href="#">Feed</a></li>
-            	<li><a href="#">Network</a></li>
-            	<li><a href="#">Jobs</a></li>
-            	<li><a href="#">Chat</a></li>
-            	<li><a href="#">Notices</a></li>
+            	<li><a href="#"><FaRss /> <span>Feed</span></a></li>
+            	<li><a href="#"><FiUsers /> <span>Network</span></a></li>
+            	<li><a href="#"><BsSuitcaseLg />  <span>Jobs</span></a></li>
+            	<li><a href="#"><BiComment /> <span>Chat</span></a></li>
+            	<li><a href="#"><FaRegBell /> <span>Notices</span></a></li>
           	</ul>
         </div>
 		<div className="box_search">
-			<input type="text" placeholder="search"/>
+			<label htmlFor="search"><FaSistrix /></label>
+			<input type="text" placeholder="Search" id="search"/>
 		</div>
 		<div className="box_account">
 			<div className="box_profile_image">
 				<img src="src/assets/profile_image.png" alt="Foto de Perfil" />
 			</div>
 			<div className="box_accounts_info">
-				<div>
+				<div className="box_info_name">
 					<h2 className="profile_name">S. Lopes</h2> 
-					<span className="text_indication">YOU</span>
+					<span className="text_indication">you</span>
 				</div>
-				<div>
-					<p>367 views today</p>
-					<span>+32 ICON</span>
+				<div className="box_info_views">
+					<p className="views_today">367 views today</p>
+					<span className="new_followers">+32 <MdArrowOutward /></span>
 				</div>
 			</div>
 		</div>
 		<div className="box_others_info">
-			ICON
-			<p>OTHER</p>
+			<div className="more_options">
+				<span>...</span>
+				<p>OTHER</p>
+			</div>
 		</div>
     </div>
   )
