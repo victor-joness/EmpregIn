f0f0f8305f73ced1efc27f71d6b99e68f0785a9f
Samuel Lopes <samuellopes12390@gmail.com>
2024-08-12 20:09:49 -0300

    feat: Feito alguns ajustes na estrutura da NavBar

--- Client/src/Components/NavBar/navbar.jsx
+++ Client/src/Components/NavBar/navbar.jsx
@@ -1,57 +1,64 @@
 import React from "react";
 
-import './navbar.css'
+import './Navbar.css'
 
 import { FiUsers } from "react-icons/fi";
 import { BsSuitcaseLg } from "react-icons/bs";
 import { FaSistrix } from "react-icons/fa6";
 import { FaRss } from "react-icons/fa6";
-import { FaRegBell } from "react-icons/fa6";
+import { FiBook } from "react-icons/fi";
 import { BiComment } from "react-icons/bi";
 import { MdArrowOutward } from "react-icons/md";
+import { IoIosLogOut } from "react-icons/io";
+import { useSelector } from "react-redux";
 
 const NavBar = () => {
-  return (
-    <div className="header">
-    	<div className="box_logo">
-        	<img src="src/assets/logo.svg" alt="Logo da Aplicação" />
-        </div>
-        <div className="box_icons_navigation">
-        	<ul>
-            	<li><a href="#"><FaRss /> <span>Feed</span></a></li>
-            	<li><a href="#"><FiUsers /> <span>Network</span></a></li>
-            	<li><a href="#"><BsSuitcaseLg />  <span>Jobs</span></a></li>
-            	<li><a href="#"><BiComment /> <span>Chat</span></a></li>
-            	<li><a href="#"><FaRegBell /> <span>Notices</span></a></li>
-          	</ul>
-        </div>
-		<div className="box_search">
-			<label htmlFor="search"><FaSistrix /></label>
-			<input type="text" placeholder="Search" id="search"/>
-		</div>
-		<div className="box_account">
-			<div className="box_profile_image">
-				<img src="src/assets/profile_image.png" alt="Foto de Perfil" />
+	const user = useSelector((state) => state.user.value)
+
+	return (
+		<div className="header">
+			<div className="box_logo">
+				<img src="Images\logo.svg" alt="Logo da Aplicação" />
 			</div>
-			<div className="box_accounts_info">
-				<div className="box_info_name">
-					<h2 className="profile_name">S. Lopes</h2> 
-					<span className="text_indication">you</span>
+			<div className="box_icons_navigation">
+				<ul>
+					<li><a href="#"><FaRss /> <span>Feed</span></a></li>
+					<li><a href="#"><FiUsers /> <span>Network</span></a></li>
+					<li><a href="#"><BsSuitcaseLg /> <span>Jobs</span></a></li>
+					<li><a href="#"><BiComment /> <span>Chat</span></a></li>
+					<li><a href="#"><FiBook /> <span>Algoritm</span></a></li>
+				</ul>
+			</div>
+			<div className="box_search">
+				<label htmlFor="search"><FaSistrix /></label>
+				<input type="text" placeholder="Search" id="search"/>
+			</div>
+			<div className="box_account">
+				<div className="box_profile_image">
+					<img 
+						src={user?.photoURL ? user.photoURL : "src/assets/profile_image.png"}
+						alt="Foto de Perfil" 
+					/>
 				</div>
-				<div className="box_info_views">
-					<p className="views_today">367 views today</p>
-					<span className="new_followers">+32 <MdArrowOutward /></span>
+				<div className="box_accounts_info">
+					<div className="box_info_name">
+						<h2 className="profile_name">{user && user.displayName}</h2> 
+						<span className="text_indication">you</span>
+					</div>
+					<div className="box_info_views">
+						<p className="views_today">367 views today</p>
+						<span className="new_followers">+32 <MdArrowOutward /></span>
+					</div>
 				</div>
 			</div>
-		</div>
-		<div className="box_others_info">
-			<div className="more_options">
-				<span>...</span>
-				<p>OTHER</p>
+			<div className="box_logout">
+				<button className="btn_logout" type="button">
+					<IoIosLogOut /> 
+					<span>Logout</span>
+				</button>
 			</div>
 		</div>
-    </div>
-  )
+	)
 };
 
 export default NavBar;
--- Client/src/Pages/Feed/Feed.jsx
+++ Client/src/Pages/Feed/Feed.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Feed_Perfil from "../../Components/Feed_Perfil/Feed_Perfil";
 import Feed_Main from "../../Components/Feed_Main/Feed_Main";
 import Feed_Recomendacao from "../../Components/Feed_Recomendacao/Feed_Recomendacao";
+import NavBar from "../../Components/NavBar/Navbar"
+
 import "./Feed.css";
 
 const Feed = (props) => {
   document.title = "Feed | Linkedin";
   return (
     <div className="container">
+      <NavBar />
       <div className="layout">
         <Feed_Main />
         <Feed_Perfil />
