cc91959b7287b013a7ce18596a5fde97dc030a31
Victor Mesquita <victorjonesmesquits@gmail.com>
2024-08-17 18:09:20 -0300

    Navegar para a home

--- Client/src/Pages/Feed/Feed.jsx
+++ Client/src/Pages/Feed/Feed.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Feed_Perfil from "../../Components/Feed_Perfil/Feed_Perfil";
 import Feed_Main from "../../Components/Feed_Main/Feed_Main";
 import Feed_Recomendacao from "../../Components/Feed_Recomendacao/Feed_Recomendacao";
 import NavBar from "../../Components/NavBar/Navbar"
 
 import "./Feed.css";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const Feed = (props) => {
   document.title = "Feed | Linkedin";
+  const user = useSelector((state) => state.user.value);
+  const navigate = useNavigate();
+
+  if(user == undefined){
+    window.location.href = "/";
+  }
+
+  console.log(user);
+
   return (
     <div className="container">
       <NavBar />
